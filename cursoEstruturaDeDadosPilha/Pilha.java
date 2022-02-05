63b615170456560e4002b49c1d17d91fb9348c4d
sheila-serafim <sheila.serafim@gmail.com>
2022-02-05 05:37:28 -0300

    iniciando o curso de pilha

---
+++ cursoEstruturaDeDadosPilha/Pilha.java
@@ -0,0 +1,18 @@
+package cursoEstruturaDeDadosPilha;
+
+import cursoEstruturaDeDados.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+	
+	public Pilha() {
+		super();
+	}
+	
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+	
+	public void empilha(T elemento) {
+		
+	}
+}
